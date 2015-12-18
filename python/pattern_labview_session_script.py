34df2c72f177bf1a7faac6459cfd0ef9b9d8753f
Antonino Ingargiola <tritemio@gmail.com>
2015-12-18 12:54:00 -0800

    Fix docstring

--- python/pattern_labview_session_script.py
+++ python/pattern_labview_session_script.py
@@ -4,17 +4,17 @@ The following variables must be set in the session before running the script.
 Arguments:
     Xm (float array): X position of the spots in LOCOS coordinates
     Ym (float array): Y posiition of the spots in LOCOS coordinates
-    phi_max (int): phi_max*pi is the max phase delay of the pattern reached
+    phi_max (float): phi_max*pi is the max phase delay of the pattern reached
         at the center of each spot. This is the initial phase set as maximum
         but the max can wrap down to a lower value.
     wl (float): laser wavelength (in m)
     f (float): single-spot lens focal length (in m)
     phase_factor (int, 0..255): 8bit level correspondic to a phase of pi
-    dark_spot (int): spot number of a spot to hide
-    darken (bool): if True hides spot `dark_spot`
+    center_spot (int): spot number of a spot to hide
+    darken_cspot (bool): if True hides spot `dark_spot`
     lw (int): line-width of the steering pattern
     vmax (int, 0..255): max level for the steering pattern
-    PhWrap (bool): if True wrap the phase above 2*pi
+    ph_wrapping (bool): if True wrap the phase above 2*pi
     pad (int): how many pixel of padding to leave around the spot pattern
         before stating the steerring pattern
     dark_all (bool): make a black pattern
